be2980f7f312c08d614977e3ce62fa325e8c0d3a
Bryant Grove <brygrove@gmail.com>
2012-10-28 00:21:39 -0700

    -added a reminder that Jre6 32 bit is needed to run the swt application. -added .gitignore so that i can ignore the local jre6 that I have in the root folder of the repo. -added run client developer.cmd in vcl-swt-client which is used to run the application from a jre from the root of the repo instead of the target folder. -updated run_client.cmd to use jre6 instead.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ vcl-app
 =======
 
 Victory Church Community Library Application
+
+The SWT Client relies on jre-6u37-windows-i586 or a 32 Bit JRE to run the 32 bit SWT native libraries.
--- vcl-app/vcl-swt-client/batch/run_client_developer.cmd
+++ vcl-app/vcl-swt-client/batch/run_client_developer.cmd
@@ -2,7 +2,7 @@
 
 echo Running VCL SWT Client ...
 
-set JAVA_32BIT_HOME=jre6
+set JAVA_32BIT_HOME=..\..\..\jre6
 
 
 %JAVA_32BIT_HOME%\bin\java -cp vcl-swt-client.jar; com.vcl.application.VclManagerApplication > log.txt 2>&1
